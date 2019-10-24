4ee832e7726516da5d2b2047798eb11d37111a81
unknown <Student@L405-9.carbon.cti.p.lodz.pl>
2019-10-24 09:33:09 +0200

    Implementation TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList newList = new ArrayList();
+        for (int i = 0; i < value.length() + 1; i++) {
+            newList.add(value.substring(i));
+        }
+        return newList;
     }
 
 }
