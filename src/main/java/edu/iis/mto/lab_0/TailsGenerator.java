4130d15109307a09ba29657af300add21e086e0f
Krystian Bąk <krystianb.bk@gmail.com>
2019-10-24 22:37:28 +0200

    Done factorization

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -7,12 +7,12 @@ public class TailsGenerator {
 
     public List<String> tails(String value) {
     	List<String> list = new ArrayList<String>();
+    	
     	list.add(value);
     	for(int i = 1; i <= value.length(); i++)
     	{
     		list.add(value.substring(i));
     	}
-
     	
         return list;
     }
