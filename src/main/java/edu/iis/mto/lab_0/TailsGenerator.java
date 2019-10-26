46814209f3f5e6afde775dceb5c4ff79a3def82c
Paweł Siciński <233008@edu.p.lodz.pl>
2019-10-26 12:26:34 +0200

    Making code compliance with tests.

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,20 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
-public class TailsGenerator {
+class TailsGenerator {
 
-    public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+    List<String> tails(@NotNull String value) {
+        List<String> output = new ArrayList<>();
+
+        for (int i = 0; i < value.length() + 1; i++) {
+            output.add(value.substring(i));
+        }
+
+        return Collections.unmodifiableList(output);
     }
 
 }
