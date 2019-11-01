0c264168e3bd043f96cb629a0ac63507efcfc570
Mike <michaj96@gmail.com>
2019-11-01 21:53:51 +0100

    adding the first element to an array, to match the second test

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> myArray = new ArrayList<>(value.length());
+        myArray.add(value);
+        return myArray;
     }
 
 }
