6f2addc35b16df46c3b06567ff5fc4716806623c
Strzepek.Aleksander <233010@edu.p.lodz.pl>
2019-11-03 00:20:09 +0100

    Username changed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -10,6 +10,7 @@ public class TailsGenerator {
     public List<String> tails(String value) {
         ArrayList<String> myList = new ArrayList<>(Collections.nCopies(value.length() + 1, ""));
         myList.set(0, value);
+		
         return myList;
     }
 
