9f1a4cd346f84f8ac7e44b5e6929127521d8bd9c
xyz <xyz@example.com>
2019-11-03 00:11:26 +0100

    lab0 tests passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -3,10 +3,14 @@ package edu.iis.mto.lab_0;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Collections;
+
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> myList = new ArrayList<>(Collections.nCopies(value.length() + 1, ""));
+        myList.set(0, value);
+        return myList;
     }
 
 }
