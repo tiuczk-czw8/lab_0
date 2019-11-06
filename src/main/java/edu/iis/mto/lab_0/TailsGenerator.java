2de68bdb06388d5c07f52fdd2211b582c5f81c25
CrossKarol <karolekkawecki@o2.pl>
2019-11-06 19:10:50 +0100

    Fix method TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,13 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        ArrayList<String> tempList = new ArrayList<String>();
+        for(int i = 0; i < value.length()+1; i++) {
+
+            tempList.add(value.substring(i));
+        }
+        return tempList;
     }
 
 }
